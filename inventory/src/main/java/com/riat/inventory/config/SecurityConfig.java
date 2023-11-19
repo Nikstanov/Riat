ea63422481a9f06da0d8a69b838e6f7328815f6d
Nikstanov <N.stahnov@gmail.com>
2023-11-20 02:21:29 +0300

    add auth with oauth2 keycloak

---
+++ inventory/src/main/java/com/riat/inventory/config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.riat.inventory.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jose.jws.SignatureAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwkSetUri;
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder.withJwkSetUri(jwkSetUri).jwsAlgorithm(SignatureAlgorithm.RS256).build();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(request ->
+                        request
+                            .requestMatchers("api/v1/items/add").authenticated()
+                            .anyRequest().permitAll());
+        http
+                .oauth2ResourceServer(configurer -> {
+                    configurer.jwt(Customizer.withDefaults());
+                });
+        http
+                .sessionManagement(configure -> {
+                    configure.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                });
+        return http.build();
+    }
+
+}
--- inventory/src/main/java/com/riat/inventory/controller/MainController.java
+++ inventory/src/main/java/com/riat/inventory/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.riat.inventory.controller;
 
+import com.riat.inventory.config.SecurityConfig;
 import com.riat.inventory.dto.ItemDto;
 import com.riat.inventory.service.ItemService;
 import com.riat.inventory.service.KafkaService;
@@ -8,6 +9,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,20 +23,26 @@ import java.util.List;
 @RestController
 @Slf4j
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/items")
 public class MainController {
 
     private final ItemService itemService;
     private  final KafkaService kafkaService;
 
     @PostMapping("/add")
+    @PreAuthorize("hasRole('Admin')")
     public ResponseEntity<HttpStatus> add(@RequestBody @Valid ItemDto item, BindingResult bindingResult) {
         if(bindingResult.hasErrors()){
             kafkaService.log("Inventory service - invalid values");
             return ResponseEntity.ok(HttpStatus.BAD_REQUEST);
         }
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        log.info(authentication.getName());
+        log.info(authentication.toString());
+        var jwt = (Jwt)authentication.getPrincipal();
+        String name = (String) jwt.getClaims().get("preferred_username");
         itemService.addNewItems(item);
-        kafkaService.log("Inventory service - new items" + item);
+        kafkaService.log("Inventory service - new items by user " + name +" "+item);
         return ResponseEntity.ok(HttpStatus.ACCEPTED);
     }
 
